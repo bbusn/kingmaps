f2605ebfd0af94fd22963d9ca215de846aeef4a6
Benoit Busnardo <benoit.busnardo@gmail.com>
2024-04-10 10:18:34 +0200

    fix visit.tsx link

--- app/ui/visitPage.tsx
+++ app/ui/visitPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ProgressBar from "./progressBar";
+import ProgressBar from "../ui/progressBar";
 import Button from "./button";
 import Image from "next/image";
 import Link from "next/link";
