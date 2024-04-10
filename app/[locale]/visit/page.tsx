b20e1f7c9531505e393fe0016f3805aa4da31d6c
Benoit Busnardo <benoit.busnardo@gmail.com>
2024-04-10 08:55:52 +0200

    visit

--- app/[locale]/visit/page.tsx
+++ app/[locale]/visit/page.tsx
@@ -6,22 +6,22 @@ import Header from "@/app/ui/layout/header";
 
 export default function Visit() {
   return (
-    <div className="flex-center-center flex-column">
+    <div className="flex-center-center flex-column ht-100-pct wd-100-pct">
       <Header />
       <div className="Visit_container">
         <div className="where">
-          <h2>VOUS ÊTES à</h2>
+          <h2 className="lilita-one">VOUS ÊTES à</h2>
           <h3 className="lilita-one entry-text-color">l'ENTRÉE</h3>
           <Image
             src="/images/visit/entry.png"
             alt="Entry image"
-            width={1000}
-            height={1000}
+            width={200}
+            height={100}
           />
-          <h4>LA Cafétéria est la prochaine salle à droite</h4>
+          <h4 className="imprima">LA Cafétéria est la prochaine salle à droite</h4>
         </div>
         <div className="Visit_container_footer">
-          <Button href="/sgm" accessKey="n" text="Suivant" type="primary" />
+          <Button href="/visit/sgm" accessKey="n" text="Suivant" type="primary" />
         </div>
         <div className="Visit_container_footer_progress">
           <ProgressBar progress={20} />
