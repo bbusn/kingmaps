9220e5b100943abfc7289aae2bbfbce6480fa1fb
Klaimand22 <clement.michellod@gmail.com>
2024-04-10 09:06:06 +0200

    Progressbar

--- app/[locale]/visit/page.tsx
+++ app/[locale]/visit/page.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import Head from "next/head";
 import ProgressBar from "../../ui/ProgressBar";
 import Button from "@/app/ui/button";
@@ -13,15 +15,22 @@ export default function Visit() {
           <h2 className="lilita-one">VOUS ÊTES à</h2>
           <h3 className="lilita-one entry-text-color">l'ENTRÉE</h3>
           <Image
-            src="/images/visit/entry.png"
+            src="/images/departements/entrance.png"
             alt="Entry image"
             width={200}
             height={100}
           />
-          <h4 className="imprima">LA Cafétéria est la prochaine salle à droite</h4>
+          <h4 className="imprima">
+            LA Cafétéria est la prochaine salle à droite
+          </h4>
         </div>
         <div className="Visit_container_footer">
-          <Button href="/visit/sgm" accessKey="n" text="Suivant" type="primary" />
+          <Button
+            href="/visit/sgm"
+            accessKey="n"
+            text="Suivant"
+            type="primary"
+          />
         </div>
         <div className="Visit_container_footer_progress">
           <ProgressBar progress={20} />
