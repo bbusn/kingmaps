7eb0f98305fab2eee92bd43d85c33e41daff5dad
Klaimand22 <clement.michellod@gmail.com>
2024-04-10 08:55:06 +0200

    end page

--- app/ui/ProgressBar.tsx
+++ app/ui/ProgressBar.tsx
@@ -1,11 +1,15 @@
 /** @format */
+import styles from "./ProgressBar.module.css";
 
 // components/ProgressBar.js
 
 const ProgressBar = ({ progress }: { progress: number }) => {
   return (
-    <div className="progress">
-      <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+    <div className="style.progress">
+      <div
+        className="style.progress-bar"
+        style={{ width: `${progress}%` }}
+      ></div>
     </div>
   );
 };
